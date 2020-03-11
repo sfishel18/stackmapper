1e2cf45f02d0ae8ebe6a019a04d15c57bf68561c
Simon Fishel <sfishel@splunk.com>
2020-03-10 20:23:27 -0700

    fix cors issue when endpoint has an error

--- site-backend/index.js
+++ site-backend/index.js
@@ -4,18 +4,22 @@ const { parseStackTrace, transformFrame, stringifyStackTrace } = require('./util
 
 exports.transform = async (req, res) => {
     res.set({
-        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Origin': 'http://www.stackmapper.com',
         'Access-Control-Allow-Headers': 'Content-Type'
     });
     if (req.method === "HEAD" || req.method === 'OPTIONS') {
       res.status(200).send();
       return;
     }
-    const { stackTrace, sourceMap } = req.body;
-    const { data } = await axios.get(sourceMap);
-    const consumer = await new sourceMapLib.SourceMapConsumer(data);
-    const parsedStackTrace = parseStackTrace(stackTrace).map(frame => transformFrame(frame, consumer));
-    res.status(200).send({ 
-      trace: stringifyStackTrace(parsedStackTrace)
-    });
+    try {
+      const { stackTrace, sourceMap } = req.body;
+      const { data } = await axios.get(sourceMap);
+      const consumer = await new sourceMapLib.SourceMapConsumer(data);
+      const parsedStackTrace = parseStackTrace(stackTrace).map(frame => transformFrame(frame, consumer));
+      res.status(200).send({ 
+        trace: stringifyStackTrace(parsedStackTrace)
+      });
+    } catch (e) {
+      res.status(400).send({ message: e.message });
+    }
 }
