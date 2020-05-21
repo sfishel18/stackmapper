2413e1010c95d32329f1156c53f398f5e9dfc06f
Simon Fishel <simonf@gmail.com>
2020-05-20 21:33:23 -0700

    split out vendor chunk in webpack config

--- site-frontend/webpack.config.js
+++ site-frontend/webpack.config.js
@@ -48,4 +48,29 @@ module.exports = {
         }),
         new webpack.HotModuleReplacementPlugin(),
     ],
+
+    optimization: {
+        splitChunks: {
+            chunks: 'initial',
+            minSize: 30000,
+            maxSize: 0,
+            minChunks: 1,
+            maxAsyncRequests: 5,
+            maxInitialRequests: 3,
+            automaticNameDelimiter: '~',
+            automaticNameMaxLength: 30,
+            name: true,
+            cacheGroups: {
+              vendors: {
+                test: /[\\/]node_modules[\\/]/,
+                priority: -10
+              },
+              default: {
+                minChunks: 2,
+                priority: -20,
+                reuseExistingChunk: true
+              }
+            }
+          }
+    }
 };
